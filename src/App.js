e22343e9a87d2a810c92b65acf84d4763ea16072
Salah Merouani <sala7merouani@gmail.com>
2022-06-02 18:45:06 +0100

    Routes updated in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 
 function App() {
   return (
-    <div className="app">
 
-      <Login />
-      
-    </div>
+    <Router>
+      <div className="app"> 
+
+      <Routes>
+        <Route path='/'  element={<Home/>} />
+        <Route path='/login' element={<Login/>}  />
+      </Routes>
+
+
+
+
+
+       </div>
+
+
+    </Router>
+
+
+    
+       
+
+     
   );
 }
 
